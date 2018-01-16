4b6cbae0bfbe697c7b97ec631f12ec631a5638fc
MxBoud <emb4733@gmail.com>
2018-01-16 19:28:37 -0400

    Initial Commit

---
+++ PartitionsDuino/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PartitionsDuino
+//
+//  Created by Maxime Boudreau2 on 18-01-16.
+//  Copyright © 2018 MxBoud. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ PartitionsDuino/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PartitionsDuino
+//
+//  Created by Maxime Boudreau2 on 18-01-16.
+//  Copyright © 2018 MxBoud. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
